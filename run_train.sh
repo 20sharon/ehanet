69ef2dfa47a005d249cbaa1a2de737e5f0e4dc6d
20sharon <106743831+20sharon@users.noreply.github.com>
2023-12-24 04:11:53 +0800

    Update run_train.sh
    # parallel

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python -u main.py --batch_size 32 --imsize 512 --arch FaceParseNet18 --parallel True #--pretrained_model 60
+CUDA_VISIBLE_DEVICES=0 python -u main.py --batch_size 32 --imsize 512 --arch FaceParseNet18 # --parallel True #--pretrained_model 60
