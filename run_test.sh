e3da7b341eb8f0e1777215105e6bed75c747a1de
20sharon <106743831+20sharon@users.noreply.github.com>
2023-12-28 03:36:41 +0800

    Update run_test.sh
    11

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python -u main.py --arch FaceParseNet18 --train False --pretrained_model 26
+CUDA_VISIBLE_DEVICES=0 python -u main.py --arch FaceParseNet18 --train False --pretrained_model 11
